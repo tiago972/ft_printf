f296662852e50863dff36a1e4cbc90e50a389a66
tiago972 <edouard.baudouin@hotmail.fr>
2019-04-29 11:52:00 +0200

    je le finis avanat jeudi

--- debug/main3.c
+++ debug/main3.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include "../libft/includes/libft.h"
 #include <limits.h>
+#include <math.h>
 
 int		main()
 {
-	long double f = 263.3;
+	float f = 263.3;
 	unsigned char *test = (unsigned char *)&f;
 	unsigned i = CHAR_BIT * sizeof(f);
 	while (--i + 1 > 0)
 	{
-		if (test[i/CHAR_BIT] & (1U << (i%CHAR_BIT)))
+		if (test[i/CHAR_BIT] & (1U << (i % CHAR_BIT)))
 			printf("1");
 		else
 			printf("0");
 	}
-	printf("\n");
-	printf("%+15Lf\n", f);
+	printf("\n%.1f\n", f);
 	return (0);
 }
